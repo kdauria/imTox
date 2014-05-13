a582ce7fd5e76f6cfcf5b2c39563881dd4a2438b
kdauria <kdauria@gmail.com>
2014-05-13 17:23:21 -0400

    finished draft plots of cck8 data

--- Scripts2/cck8.R
+++ Scripts2/cck8.R
@@ -30,7 +30,7 @@ for( j in 2:ncol(dat) ) {
   }
 }
 
-#################### Make data matrix
+#################### Look at the raw data
 ggmat = function(wells) {
   groups = list(cond="by.concentrations",toxin="by.compounds")
   mdat = melt.wellList.wgroups(wells,groupings=groups,ID="final")
@@ -39,16 +39,51 @@ ggmat = function(wells) {
 }
 
 # Take a look at the raw absorbance data for CCK8
-ggplot(ggmat(wells),aes(x=factor(time),y=value)) + geom_point() + facet_wrap( ~ cond)
+p = ggplot(ggmat(wells),aes(x=factor(time),y=value)) + geom_point() + facet_wrap( ~ cond)
 
 subset = retrieveWells(wells,compounds=c("A","lysis","control"))
-ggplot(ggmat(subset),aes(x=factor(time),y=value)) + geom_point() + facet_wrap( ~ cond)
+p %+% ggmat(subset)
 
 subset = retrieveWells(wells,compounds=c("B","lysis","control"))
-ggplot(ggmat(subset),aes(x=factor(time),y=value)) + geom_point() + facet_wrap( ~ cond)
+p %+% ggmat(subset)
 
 subset = retrieveWells(wells,compounds=c("mB","lysis","control"))
-ggplot(ggmat(subset),aes(x=factor(time),y=value)) + geom_point() + facet_wrap( ~ cond)
+p %+% ggmat(subset)
+
+
+############### Transform the data given the controls
+x = ggmat(wells)
+
+# make a replicate # column
+allcond = interaction(x$time,x$cond)
+x$reps = unlist( mapply( seq, 1, rle(as.character(allcond))$lengths ) )
+
+# cast the data with one column per time point (#rows = #wells)
+y = cast(x, cond + reps + toxin ~ time )
+
+# sweep out the lysis buffer controls
+vc = 4:6 # value columns
+y[,vc] = sweep(y[,vc],2,colMeans( y[y$cond=="lysis-1",vc] ))
+
+# normalize to controls
+y[,vc] = sweep( y[,vc], 2, colMeans( y[y$cond=="control-1",vc] ), "/" )
+
+############## Plot the transformed data
+z = melt(y,measure.vars=c("1","4","24"))
+p %+% z
+
+a = z[ z$toxin %in% c("A","lysis","control"), ]
+p %+% a
+
+a = z[ z$toxin %in% c("B","lysis","control"), ]
+p %+% a
+
+a = z[ z$toxin %in% c("mB","lysis","control"), ]
+p %+% a
+
+
+
+
 
 
 
