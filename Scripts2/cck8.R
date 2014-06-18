10115fa82cde0442c383be6de1993f680c311437
kdauria <kdauria@gmail.com>
2014-06-18 00:34:15 -0400

    bar chart cck8 figure

--- Scripts2/cck8.R
+++ Scripts2/cck8.R
@@ -4,6 +4,7 @@ library(wellz)
 library(reshape)
 library(ggplot2)
 library(data.table)
+library(stringr)
 
 ################### Parse annotations and data
 my_parse_fun = function( fpath ) {
@@ -25,65 +26,31 @@ controls = cast(melt_wellList( select(x,"control") ), t~., fun.aggregate=mean )[
 blanks = cast(melt_wellList( select(x,"blank") ), t~., fun.aggregate=mean )[,2]
 for( i in seq_along(x)) vdata(x[[i]]) = (vdata(x[[i]])-blanks)/(controls-blanks)
 x = add_spline(x) # need to update the spline whenever the data is modified
-plot(x,replicates=TRUE, sd=FALSE, color="compound")
+plot(x,replicates=TRUE, sd=TRUE, color="compound") + facet_wrap(~compound)
 
+# Select a subset of the data
+y = select(x, "A[10-100] | B[10-100] | mB[10-100] | control | lysis")
+z = select(y, "!(A & mB) & !(B & mB)")
 
-############## Reorganize the data
-dat = melt_wellList(x)
-p = param_matrix(x)
-p$A.conc = group(x,"concentration",compound="A")
-p$B.conc = group(x,"concentration",compound="B")
-p$mB.conc = group(x,"concentration",compound="mB")
-p$names = group(x,"compound")
-p$tox.conc = p$A.conc + p$B.conc + p$mB.conc*as.numeric(p$names=="mB")
+# Reorganize the data so that bar plots can be made
+dat = melt_wellList_params(z,color="concentration")
 
-p$comps = p$names
-temp = p$names; yn = p$names %in% c("A+mB","B+mB")
-p$comps[yn] = paste( p$names[yn], p$mB.conc[yn], sep="." )
-
-dat = dat[p]
-
-
-############  Plot the data
-p = ggplot(dat, aes(x=factor(log10(tox.conc)),y=value,fill=comps, color=comps)) + facet_wrap(~t) + 
-  stat_summary(fun.y="mean", geom="bar", position="dodge", alpha=0.4) +
-  geom_point(position=position_dodge(width=0.8))
-p
-
-############# Plot subsets of the data
-dat2 = dat[ dat$tox.conc>1, ]
-p %+% dat2
-
-dat3 = dat[ dat$tox.conc<=1, ]
-p %+% dat3
-
-########## The plotting is ugly because
-# position_dodge drops a bar if it isn't in one of the
-# factors on the x-axis. Therefore, I'm going to need to
-# calculate the summary statistics beforehand and then do the
-# plotting
-
-# What are the keys that separate the data values into groups?
-# tox.conc, comps, and t
-sum.stats = as.data.frame( dat[,mean(value),by=list(tox.conc,comps,t)] )
-sum.stats[order(sum.stats$t,sum.stats$comps),]
-
-
-groups = with(sum.stats, interaction(comps,tox.conc,t))
-length( levels(factor(sum.stats$comps))) # 9
-length( levels(factor(sum.stats$tox.conc))) # 6
-length( levels(factor(sum.stats$t))) # 3
-# Meh this is possibly 9*6*3 = 162 groups. It makes
-# more sense to pick and choose what I want to show
-# because showing all is just too complicated
-
-
-
-
-
-
-
-
+pdf("./bars.pdf",width=3,height=7)
+ggplot(data=dat, aes(x=factor(t),y=value)) +
+  stat_summary(fun.y="mean", geom="bar") +
+  facet_wrap(~concentration,ncol=2) +
+  geom_point(aes(color=concentration)) +
+  theme(strip.background=element_blank(),
+        panel.border=element_blank(),
+        panel.grid.major.x=element_blank(),
+        panel.grid.minor.y=element_blank(),
+        panel.grid.major.y=element_line(colour="lightblue"),
+        panel.background=element_blank(),
+        legend.position="none",
+        axis.text=element_blank(),
+        axis.ticks=element_blank(),
+        axis.title=element_blank())
+dev.off()
 
 
 
